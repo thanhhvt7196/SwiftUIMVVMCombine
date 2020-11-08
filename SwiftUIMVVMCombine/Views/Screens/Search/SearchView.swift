e853b608ab6f83f86b5e29d14ab8366b49309b82
thanh tien <thanhhvt7196@gmail.com>
2020-11-08 11:58:29 +0700

    add swiftgen and tabbar

---
+++ SwiftUIMVVMCombine/Views/Screens/Search/SearchView.swift
@@ -0,0 +1,20 @@
+//
+//  SearchView.swift
+//  SwiftUIMVVMCombine
+//
+//  Created by thanh tien on 11/8/20.
+//
+
+import SwiftUI
+
+struct SearchView: View {
+    var body: some View {
+        Text("SearchView")
+    }
+}
+
+struct SearchView_Previews: PreviewProvider {
+    static var previews: some View {
+        SearchView()
+    }
+}
